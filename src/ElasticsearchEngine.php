b366084538e155adef6234a4f2494bfa13ec9779
seongbae <bae.seong@hotmail.com>
2016-10-07 22:51:06 -0400

    column search

--- src/ElasticsearchEngine.php
+++ src/ElasticsearchEngine.php
@@ -131,7 +131,7 @@ class ElasticsearchEngine extends Engine
             'body' => [
                 'query' => [
                     'bool' => [
-                        'must' => [['query_string' => [ 'query' => "*{$builder->query}*"]]]
+                        'must' => [['query_string' => [ 'query' => "ingredients:*{$builder->query}*"]]]
                     ]
                 ]
             ]
